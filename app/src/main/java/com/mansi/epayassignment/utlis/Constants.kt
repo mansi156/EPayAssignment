8167f0fcd6c82d9f5e1badbd977e2f5b1363cdaa
mansisharma <mansi1561995@gmail.com>
2022-01-07 02:52:52 +0530

    Add API key

--- app/src/main/java/com/mansi/epayassignment/utlis/Constants.kt
+++ app/src/main/java/com/mansi/epayassignment/utlis/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val BASE_URL ="https://api.openweathermap.org/data/2.5/"
         const val IMAGE_URL ="http://openweathermap.org/img/w/"
-        const val API_KEY = "9b8cb8c7f11c077f8c4e217974d9ee40" //"78a07164952e030a671b9350f648cd70"
+        const val API_KEY = "78a07164952e030a671b9350f648cd70"
         const val EXCLUDE = "current,minutely,alerts"
         const val TITLE = "title"
     }
